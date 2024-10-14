ad4f57cf1dc64d6bf40222673b89c7fcea57bf8f
ai-man24 <aiman.azees24@gmail.com>
2024-10-15 01:38:40 +0530

    feat: Added gravity

--- Assets/Scripts/Editor/Common.cs
+++ Assets/Scripts/Editor/Common.cs
@@ -4,7 +4,9 @@ namespace EditorPlatformer.Editor
 {
     public struct Info
     {
-        public const float PLAYER_SPEED = 5f;
+        public const float PLAYER_SPEED = 10f;
+        public const float GRAVITY = 0.4f;
+        public const float TICK_RATE = 0.02f;
         
         public static readonly Vector2 PlayerSize = new Vector2(50f, 50f);
         public static readonly Color PlayerColor = new Color(1f, 0f, 0f, 0.5f);
--- Assets/Scripts/Editor/MainWindow.cs
+++ Assets/Scripts/Editor/MainWindow.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace EditorPlatformer.Editor
 {
@@ -29,9 +32,18 @@ namespace EditorPlatformer.Editor
         }
 
         public int windowCount => m_windows.Count;
-        
+
         [SerializeField] private List<PlatformWindow> m_windows = new List<PlatformWindow>();
         [SerializeField] private Vector2 m_playerPosition = new Vector2(100f, 100f);
+
+        private Stopwatch m_stopwatch;
+        private KeyCode m_lastPressedKey;
+
+        private void OnEnable()
+        {
+            m_stopwatch = new Stopwatch();
+            m_stopwatch.Start();
+        }
 
         private void Add(PlatformWindow window)
         {
@@ -50,77 +62,108 @@ namespace EditorPlatformer.Editor
         private void OnGUI()
         {
             Focus();
-
+            
             if (Event.current.type == EventType.KeyDown)
             {
-                var targetPosition = m_playerPosition;
-                switch (Event.current.keyCode)
-                {
-                    case KeyCode.W:
-                    case KeyCode.UpArrow:
-                        targetPosition.y -= Info.PLAYER_SPEED;
-                        break;
-                    case KeyCode.S:
-                    case KeyCode.DownArrow:
-                        targetPosition.y += Info.PLAYER_SPEED;
-                        break;
-                    case KeyCode.A:
-                    case KeyCode.LeftArrow:
-                        targetPosition.x -= Info.PLAYER_SPEED;
-                        break;
-                    case KeyCode.D:
-                    case KeyCode.RightArrow:
-                        targetPosition.x += Info.PLAYER_SPEED;
-                        break;
-                }
-
-                
-                var isLeftTopBound = false;
-                var isLeftBottomBound = false;
-                var isRightTopBound = false;
-                var isRightBottomBound = false;
-                var playerSize = Info.PlayerSize;
-                for (var index = 0; index < m_windows.Count; index++)
-                {
-                    var window = m_windows[index];
-                    var windowRect = window.position;
-                    windowRect.position = new Vector2(windowRect.position.x, windowRect.position.y - 18f);
-                    windowRect.size = new Vector2(windowRect.size.x, windowRect.size.y + 18f);
-                    var leftTop = new Vector2(targetPosition.x, targetPosition.y);
-                    var rightTop = new Vector2(targetPosition.x + playerSize.x, targetPosition.y);
-                    var leftBottom = new Vector2(targetPosition.x, targetPosition.y + playerSize.y);
-                    var rightBottom = new Vector2(targetPosition.x + playerSize.x, targetPosition.y + playerSize.y);
-                    
-                    isLeftTopBound |= windowRect.Contains(leftTop);
-                    isLeftBottomBound |= windowRect.Contains(leftBottom);
-                    isRightTopBound |= windowRect.Contains(rightTop);
-                    isRightBottomBound |= windowRect.Contains(rightBottom);
-                }
-
-                var isWithinOneOrManyWindows = isLeftTopBound && isLeftBottomBound && isRightTopBound && isRightBottomBound;
-                if (isWithinOneOrManyWindows)
-                {
-                    m_playerPosition = targetPosition;
-                }
-                
+                m_lastPressedKey = Event.current.keyCode;
             }
             
+            if(m_stopwatch.ElapsedMilliseconds < Info.TICK_RATE * 1000)
+                return;
+
+            m_stopwatch.Start();
+
             Tick();
+
+            m_lastPressedKey = KeyCode.None;
         }
 
         private void Tick()
         {
+            var targetPosition = m_playerPosition;
+
+            switch (m_lastPressedKey)
+            {
+                // case KeyCode.W:
+                // case KeyCode.UpArrow:
+                //     targetPosition.y -= Info.PLAYER_SPEED;
+                //     break;
+                // case KeyCode.S:
+                // case KeyCode.DownArrow:
+                //     targetPosition.y += Info.PLAYER_SPEED;
+                //     break;
+                case KeyCode.A:
+                case KeyCode.LeftArrow:
+                    targetPosition.x -= Info.PLAYER_SPEED;
+                    break;
+                case KeyCode.D:
+                case KeyCode.RightArrow:
+                    targetPosition.x += Info.PLAYER_SPEED;
+                    break;
+            }
+
+            CalculateBounds(targetPosition, out var isLeftTopWithinBounds, out var isLeftBottomWithinBounds, out var isRightTopWithinBounds, out var isRightBottomWithinBounds);
+
+            var isInAir = isLeftBottomWithinBounds && isRightBottomWithinBounds;
+            var gravityMultiplier = isInAir ? 1f : 0f;
+            targetPosition.y += Info.GRAVITY * gravityMultiplier;
+
+            // targetPosition = ClampPlayerPosition(targetPosition);
+            
+            if(isLeftTopWithinBounds && isRightTopWithinBounds)
+                m_playerPosition = targetPosition;
+
+            // CalculateBounds(targetPosition, out isLeftTopWithinBounds, out isLeftBottomWithinBounds, out isRightTopWithinBounds, out isRightBottomWithinBounds);
+            //
+            // var isWithinOneOrManyWindows = isLeftTopWithinBounds && isLeftBottomWithinBounds && isRightTopWithinBounds && isRightBottomWithinBounds;
+            // if (isWithinOneOrManyWindows)
+            // {
+            //     m_playerPosition = targetPosition;
+            // }
+
             var playerArgs = new PlayerArgs
             {
                 center = m_playerPosition,
                 size = Info.PlayerSize
             };
-            
+
             for (var index = 0; index < m_windows.Count; index++)
             {
                 var window = m_windows[index];
                 window.Tick(playerArgs);
             }
+        }
+
+        private void CalculateBounds(Vector2 targetPosition, out bool isLeftTopWithinBounds, out bool isLeftBottomWithinBounds, out bool isRightTopWithinBounds, out bool isRightBottomWithinBounds)
+        {
+            isLeftTopWithinBounds = false;
+            isLeftBottomWithinBounds = false;
+            isRightTopWithinBounds = false;
+            isRightBottomWithinBounds = false;
+            var playerSize = Info.PlayerSize;
+            for (var index = 0; index < m_windows.Count; index++)
+            {
+                var window = m_windows[index];
+                var windowRect = window.position;
+                windowRect.position = new Vector2(windowRect.position.x, windowRect.position.y - 18f);
+                windowRect.size = new Vector2(windowRect.size.x, windowRect.size.y + 18f);
+                var leftTop = new Vector2(targetPosition.x, targetPosition.y);
+                var rightTop = new Vector2(targetPosition.x + playerSize.x, targetPosition.y);
+                var leftBottom = new Vector2(targetPosition.x, targetPosition.y + playerSize.y);
+                var rightBottom = new Vector2(targetPosition.x + playerSize.x, targetPosition.y + playerSize.y);
+
+                isLeftTopWithinBounds |= windowRect.Contains(leftTop);
+                isLeftBottomWithinBounds |= windowRect.Contains(leftBottom);
+                isRightTopWithinBounds |= windowRect.Contains(rightTop);
+                isRightBottomWithinBounds |= windowRect.Contains(rightBottom);
+            }
+        }
+
+        private Vector2 ClampPlayerPosition(Vector2 playerPosition)
+        {
+            playerPosition.x = Mathf.Clamp(playerPosition.x, 0f, Screen.currentResolution.width - Info.PlayerSize.x);
+            playerPosition.y = Mathf.Clamp(playerPosition.y, 0f, Screen.currentResolution.height - Info.PlayerSize.y);
+            return playerPosition;
         }
     }
 }
