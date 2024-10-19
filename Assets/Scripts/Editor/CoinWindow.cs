8c6a3023c48e22c32c15ec7c8b29448dc76fc7d8
ai-man24 <aiman.azees24@gmail.com>
2024-10-20 03:36:49 +0530

    feat: Updated coin collection window and logic

--- Assets/Scripts/Editor/CoinWindow.cs
+++ Assets/Scripts/Editor/CoinWindow.cs
@@ -8,16 +8,33 @@ namespace EditorPlatformer.Editor
         [SerializeField]
         private bool m_isCollected;
         
-        protected override void OnGUI()
+        [SerializeField]
+        private Rect m_coinRect;
+
+        public override void Tick(PlayerArgs args)
         {
-            base.OnGUI();
+            base.Tick(args);
             
             var windowRect = this.position;
             var centerX = windowRect.width * 0.5f;
             var centerY = windowRect.height - Info.COIN_SIZE;
             var coinRect = new Rect(centerX - Info.COIN_SIZE * 0.5f, centerY - Info.COIN_SIZE * 0.5f, Info.COIN_SIZE, Info.COIN_SIZE);
+            m_coinRect = coinRect;
+            
+            if (coinRect.Overlaps(m_playerRectInWindow))
+            {
+                m_isCollected = true;
+            }
+        }
+        
+        protected override void OnGUI()
+        {
+            base.OnGUI();
 
-            EditorGUI.DrawRect(coinRect, Info.CoinColor);
+            if (!m_isCollected)
+            {
+                EditorGUI.DrawRect(m_coinRect, Info.CoinColor);
+            }
         }
     }
 }
--- Assets/Scripts/Editor/LevelWindow.cs
+++ Assets/Scripts/Editor/LevelWindow.cs
@@ -13,23 +13,25 @@ namespace EditorPlatformer.Editor
     public abstract class LevelWindow : EditorWindow, ILevelWindow
     {
         [SerializeField] private PlayerArgs m_playerArgs;
+        
+        [SerializeField] protected Rect m_playerRectInWindow;
 
         public virtual void Tick(PlayerArgs args)
         {
             m_playerArgs = args;
-        }
-
-        protected virtual void OnGUI()
-        {
+            
             var playerCenter = m_playerArgs.center;
             var playerSize = m_playerArgs.size;
             var playerRect = new Rect(playerCenter.x - playerSize.x / 2, playerCenter.y - playerSize.y / 2, playerSize.x, playerSize.y);
 
             var windowPosition = position.position;
 
-            var playerRectInWindow = new Rect(playerRect.position.x - windowPosition.x, playerRect.position.y - windowPosition.y, playerRect.width, playerRect.height);
+            m_playerRectInWindow = new Rect(playerRect.position.x - windowPosition.x, playerRect.position.y - windowPosition.y, playerRect.width, playerRect.height);
+        }
 
-            EditorGUI.DrawRect(playerRectInWindow, Info.PlayerColor);
+        protected virtual void OnGUI()
+        {
+            EditorGUI.DrawRect(m_playerRectInWindow, Info.PlayerColor);
 
             Repaint();
         }
